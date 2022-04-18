60c794ed9c17f34da1db01db480e649a6f4f7ff3
thisKai <thiskaiguy@gmail.com>
2022-04-18 17:39:07 +0100

    Create macros to concatenate last ident in paths

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,129 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use proc_macro::{Ident, Punct, Spacing, TokenStream, TokenTree};
+use std::iter;
+
+#[proc_macro]
+pub fn insert_before_path_tail(tokens: TokenStream) -> TokenStream {
+    let mut tokens = tokens.into_iter();
+
+    let ident = parse_first_arg(&mut tokens);
+    parse_comma(&mut tokens);
+    let mut path = parse_second_arg(&mut tokens);
+
+    let new_last_segment = {
+        let last_segment = path.segments.pop().unwrap();
+
+        let mut ident_string = ident.to_string();
+        ident_string.push_str(&last_segment.to_string());
+
+        Ident::new(&ident_string, last_segment.span())
+    };
+
+    path.segments
+        .into_iter()
+        .map(TokenTree::Ident)
+        .map(|tt| {
+            iter::once(tt).chain(
+                [
+                    Punct::new(':', Spacing::Joint),
+                    Punct::new(':', Spacing::Alone),
+                ]
+                .map(TokenTree::Punct),
+            )
+        })
+        .flatten()
+        .chain(iter::once(TokenTree::Ident(new_last_segment)))
+        .collect()
+}
+
+#[proc_macro]
+pub fn insert_after_path_tail(tokens: TokenStream) -> TokenStream {
+    let mut tokens = tokens.into_iter();
+
+    let ident = parse_first_arg(&mut tokens);
+    parse_comma(&mut tokens);
+    let mut path = parse_second_arg(&mut tokens);
+
+    let new_last_segment = {
+        let last_segment = path.segments.pop().unwrap();
+
+        let mut ident_string = last_segment.to_string();
+        ident_string.push_str(&ident.to_string());
+
+        Ident::new(&ident_string, last_segment.span())
+    };
+
+    path.segments
+        .into_iter()
+        .map(TokenTree::Ident)
+        .map(|tt| {
+            iter::once(tt).chain(
+                [
+                    Punct::new(':', Spacing::Joint),
+                    Punct::new(':', Spacing::Alone),
+                ]
+                .map(TokenTree::Punct),
+            )
+        })
+        .flatten()
+        .chain(iter::once(TokenTree::Ident(new_last_segment)))
+        .collect()
+}
+
+
+fn parse_comma(tokens: &mut <proc_macro::TokenStream as IntoIterator>::IntoIter) {
+    match tokens.next() {
+        Some(TokenTree::Punct(punct))
+            if punct.spacing() == Spacing::Alone && punct.as_char() == ',' => {}
+        _ => panic!("Expected comma"),
     }
+}
+
+fn parse_first_arg(tokens: &mut <proc_macro::TokenStream as IntoIterator>::IntoIter) -> Ident {
+    while let Some(token_tree) = tokens.next() {
+        if let TokenTree::Ident(ident) = token_tree {
+            return ident;
+        } else {
+            panic!("Expected ident");
+        }
+    }
+    panic!("Expected ident");
+}
+
+fn parse_second_arg(tokens: &mut <proc_macro::TokenStream as IntoIterator>::IntoIter) -> Path {
+    let mut segments = Vec::new();
+
+    let mut look_for_segment = true;
+    while let Some(token_tree) = tokens.next() {
+        if look_for_segment {
+            match token_tree {
+                TokenTree::Ident(ident) => {
+                    segments.push(ident);
+                    look_for_segment = false;
+                }
+                _ => panic!("Expected ident"),
+            }
+        } else {
+            match token_tree {
+                TokenTree::Punct(punct)
+                    if punct.spacing() == Spacing::Joint && punct.as_char() == ':' =>
+                {
+                    match tokens.next() {
+                        Some(TokenTree::Punct(punct))
+                            if punct.spacing() == Spacing::Alone && punct.as_char() == ':' =>
+                        {
+                            look_for_segment = true;
+                        }
+                        _ => panic!("Expected ::"),
+                    }
+                }
+                _ => panic!("Expected ::"),
+            }
+        }
+    }
+
+    Path { segments }
+}
+#[derive(Debug)]
+struct Path {
+    segments: Vec<Ident>,
 }
---
+++ tests/concat_path_tail.rs
@@ -0,0 +1,22 @@
+use utility_macros::{insert_after_path_tail, insert_before_path_tail};
+
+#[test]
+fn insert_before_path_tail_macro() {
+    mod module {
+        pub fn set_prop() -> bool {
+            true
+        }
+    }
+    let func = insert_before_path_tail!(set_, module::prop);
+    assert_eq!(func(), module::set_prop());
+}
+#[test]
+fn insert_after_path_tail_macro() {
+    mod module {
+        pub fn prop_set() -> bool {
+            true
+        }
+    }
+    let func = insert_after_path_tail!(_set, module::prop);
+    assert_eq!(func(), module::prop_set());
+}
